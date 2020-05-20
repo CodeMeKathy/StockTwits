d9cb47d753c9207ef383055a9e9443e62c7be76f
Kathy * <Kathy@KathyLambert.me>
2020-05-20 01:23:30 -0400

    Feature(TweetContainer): Add input field with button to component

--- src/Components/TweetContainer/TweetContainer.js
+++ src/Components/TweetContainer/TweetContainer.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react'
-// import './TweetContainer.css'
+import './TweetContainer.css'
 
 // React Bootstrap Component Imports
 import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 const TweetContainer = props => {
   const [symbolSearch, setSymbolSearch] = useState('')
   return (
     <Container fluid className='TweetContainer'>
       <h2>TweetContainer</h2>
+      <Row>
+        <Col>
+          <div className='form-group col-md-6'>
+            <input
+              name='symbols'
+              type='text'
+              className='form-control'
+              placeholder='NKE, SPOT, GOOG'
+              // value={symbols}
+            ></input>
+            <button className='btn'>Get Tweet</button>
+          </div>
+        </Col>
+        <Col>Tweets</Col>
+      </Row>
     </Container>
   )
 }
