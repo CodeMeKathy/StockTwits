cd5d5f0cc0460f318ba672d624a7da8b3a0ca2ef
Kathy * <Kathy@KathyLambert.me>
2020-05-19 21:21:57 -0400

    Refractor(App.js): Add React Bootstrap components to App.js with background image and remove initial placeholder text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>Hello, world!</h1>
-      </div>
+      <Container fluid className='App'>
+        <Row>
+          <Col>
+            <h1>Hello, world!</h1>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
